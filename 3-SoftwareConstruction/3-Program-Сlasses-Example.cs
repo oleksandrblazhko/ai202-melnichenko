082614c6b8b44e910855a38ed4ba9c01f950df67
Van Darkholme <79746684+lllDungeonMasterlll@users.noreply.github.com>
2022-11-26 16:16:06 +0200

    Lab9

---
+++ 3-SoftwareConstruction/3-Program-Сlasses-Example.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public List<Message> UserMessages { get; set; }
+}
+
+public class Message
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public int Id { get; set; }
+    public string message { get; set; }
+    public User sender  { get; set; }
+    public User receiver { get; set; }
+    public DateTime CreatedDate { get; } = DateTime.Now;
+}
---
+++ 3-SoftwareConstruction/5-Program-Methods-Using-Example.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleApp1.DataAccsess;
+using ConsoleApp1.Models;
+
+SendMessage db = new SendMessage();
+
+User newData = new User()
+{
+    LastName = "Herringhton",
+    FirstName = "Billy"
+   
+};
+await db.Create(newData);
+var list = await db.GetAll();
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+//Code -1
+Message MessageData = new Message()
+{
+    sender = newData,
+    receiver = newData,
+    message = "without frezzer iteruption lets celebraite and ..." 
+
+};
+var number = await db.SendMes(MessageData);
+
+Console.WriteLine(MessageData);
+
+User newData2 = new User()
+{
+    Id=0,
+    LastName = "Rambo",
+    FirstName = "Jhon"
+
+};
+
+Message MessageData2 = new Message()
+{
+    sender = newData,
+    receiver = newData,
+    message = null 
+
+};
+var number2 = await db.SendMes(MessageData2);
+
+Console.WriteLine(MessageData2);
+
